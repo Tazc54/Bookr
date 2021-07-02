6dd5645e064bc11489b85ed4ca733afae116fba7
tazc54 <tazc54@gmail.com>
2021-07-02 17:04:03 -0500

    Updating test_views

--- bookr/bookr/settings.py
+++ bookr/bookr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secretkey import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@=h!##0m-9&3fe%m4$j3uigo!jr6%-5&sm5acrjy)f9#3)b#7'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
